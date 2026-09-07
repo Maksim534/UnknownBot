e1b8287f59e558c5314b9349a18707d38e14599a
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-09-08 02:19:41 +0300

    Update cards_data.py

--- data/cards_data.py
+++ data/cards_data.py
@@ -14,7 +14,7 @@ cards = {
         "name": "Си-Си",
         "rarity": "обычная",
         "image_url": "https://i.ibb.co/3ZkyrB1/6f67f972e5bc635798336e2ab125cfa8.jpg",
-        "sell_price": 200,
+        "sell_price": 50,
         "case_type": "обычный",
         "is_event": False,
         "description": "«От того, чем по-настоящему дорожишь, лучше держаться подальше»"
@@ -23,9 +23,72 @@ cards = {
         "name": "Рудеус Грейрат",
         "rarity": "обычная",
         "image_url": "https://i.ibb.co/Nnmt6Bx8/image.png",
-        "sell_price": 200,
+        "sell_price": 50,
         "case_type": "обычный",
         "is_event": False,
         "description": "«Некоторые люди просто предпочитают видеть мир более драматичным. Факт более странен, чем вымысел. Реальность странна, беспомощна и разочаровывает»"
+    },
+    4: {
+        "name": "Тандзиро Камадо",
+        "rarity": "редкая",
+        "image_url": "https://i.ibb.co/4g1L7Cxj/image.png",
+        "sell_price": 100,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "«Я никогда не потерплю поражения!»"
+    },
+    5: {
+        "name": "Саске Учиха",
+        "rarity": "редкая",
+        "image_url": "https://i.ibb.co/ZwvBGyQ/image.png",
+        "sell_price": 100,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "«То, что у меня есть, — это не мечта, а амбиция»"
+    },
+    6: {
+        "name": "Мадара Учиха",
+        "rarity": "эпическая",
+        "image_url": "https://i.ibb.co/bRHXt0sp/image.png",
+        "sell_price": 200,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "«Чем дольше ты живёшь, тем больше осознаёшь, что мир состоит из боли, страдания и пустоты»"
+    },
+    7: {
+        "name": "Хитоши Шинсо",
+        "rarity": "редкая",
+        "image_url": "https://i.ibb.co/4wnQ05sV/image.png",
+        "sell_price": 100,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "«Может, на этот раз я проиграл, но… Я не сдамся. Чего бы мне этого ни стоило, я попаду на геройский факультет и стану лучшим героем, чем все вы»"
+    },
+    8: {
+        "name": "Нейто Монома",
+        "rarity": "обычная",
+        "image_url": "https://i.ibb.co/T9grvPd/image.png",
+        "sell_price": 50,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "«Парадокс в том, что для того, чтобы стать героями, мы должны вести себя негероически. В противном случае мы никогда не равняемся с теми, кто способен на величие. Недостаточно подражать нашим кумирам». "
+    },
+    9: {
+        "name": "Йо Шиндо",
+        "rarity": "эпическая",
+        "image_url": "https://i.ibb.co/PZYKWHn6/image.png",
+        "sell_price": 200,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "«Я предпочитаю, чтобы меня называли тактичным» "
+    },
+    10: {
+        "name": "Курису Макисе",
+        "rarity": "легендарная",
+        "image_url": "https://i.ibb.co/KxRDsSTT/image.png",
+        "sell_price": 400,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "«Наука — это прежде всего уверенность в себе. Если я не превращу невозможное в реальность, то останусь всего лишь второсортным учёным»"
     },
 }
