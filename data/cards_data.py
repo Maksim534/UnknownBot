cae023c5aaabe1120aa9b19d3f698a9f05b588c4
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-09-07 23:31:10 +0300

    Update and rename cards.json to cards_data.py

---
+++ data/cards_data.py
@@ -0,0 +1,23 @@
+# data/cards_data.py
+
+cards = {
+    1: {
+        "name": "Рей Аянами",
+        "rarity": "обычная",
+        "image_url": "https://i.ibb.co/zTRRk53t/e6c69869436ef88f4b4fb24fbdbc7c4d.jpg",
+        "sell_price": 50,
+        "case_type": "обычный",
+        "is_event": False,
+        "description": "Она — первый из «Детей», способных управлять боевыми биороботами «Евангелионами»."
+    },
+    2: {
+        "name": "Си-Си",
+        "rarity": "редкая",
+        "image_url": "https://i.ibb.co/3ZkyrB1/6f67f972e5bc635798336e2ab125cfa8.jpg",
+        "sell_price": 200,
+        "case_type": "редкий",
+        "is_event": False,
+        "description": "Героиня из виртуального мира."
+    },
+    # ... добавляйте сколько угодно карт
+}
